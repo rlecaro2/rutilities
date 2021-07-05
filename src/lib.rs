5c13b43a00ab6561343aae941e144d2459924ef9
Rafael Lecaros <rafael.lecaros@gmail.com>
2021-07-05 14:07:51 -0400

    add clean utility obviously modules hate me

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
-pub fn clean() {}
+mod utilities;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_removes_dots() {
-        assert_eq!(clean(), ());
-    }
-}
+pub use utilities::clean;
